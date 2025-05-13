bad95aee31a175ca4029092726b579bf62175430
SUH1992 <z3space.suhjihwa@gmail.com>
2025-05-13 22:51:34 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductSelector from './ProductSelector';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <ProductSelector />
+    </div>
+  );
+}
+
+export default App;
---
+++ ProductSelector.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+
+/* 코드 내용 생략 – 실제로는 사용자가 준 긴 ProductSelector 컴포넌트 전체가 여기에 들어감 */
+
+export default ProductSelector;
